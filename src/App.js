7fbfd2ec86b2c12f7513afe921fe8bc71fd9f4c2
roshankhaire <roshankhaire1992@gmail.com>
2022-12-20 08:54:08 +0530

    Ref Controlled vs Uncontrolled component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import AddUser from './components/Users/AddUser';
+
 import UsersList from './components/Users/UsersList';
 import React,{useState,Fragment} from 'react';
 
 function App() {
   const [usersList,setUserslist]=useState([]);
-  const addUserHandler=(uName,uAge)=>{
+  const addUserHandler=(uName,uAge,ucollage)=>{
     setUserslist((prevUsersList)=>{
-      return[...prevUsersList,{name:uName} ,{age:uAge}]
+      return[...prevUsersList,{name:uName} ,{age:uAge},{collage:ucollage}]
     })
 
   }
